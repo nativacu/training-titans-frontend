633863b06c153ec0c9aa7a1b5b57486c0ee8dcc9
nativacu <nativacu@gmail.com>
2023-07-07 17:16:03 -0400

    feat: adding i18n package and creating sample translation

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import {I18nextProvider} from "react-i18next";
+import i18next from "i18next";
+import common_en from "./assets/translations/en/common.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: 'en',
+  resources: {
+      en: {
+          common: common_en
+      },
+  },
+});
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
