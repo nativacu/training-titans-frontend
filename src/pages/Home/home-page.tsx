7c1fc8af3f8401d2fdc114c617e742bfc9b9089c
nativacu <nativacu@gmail.com>
2023-07-10 09:16:06 -0400

    chore: code cleanup

---
+++ src/pages/Home/home-page.tsx
@@ -0,0 +1,22 @@
+import { IonContent, IonHeader, IonPage } from '@ionic/react';
+import { TechStackCard } from '../../components/TechStackCard/tech-stack-card';
+import './home-page.scss';
+
+const HomePage: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader className="no-shadow-header">
+      </IonHeader>
+      <IonContent fullscreen>
+        <div className='ion-flex'>
+          <TechStackCard id={1} label='Test Card' onClick={(id) => console.log(id)} trainingCount={0} footer={(<>footer</>)}>
+            dajshiahd
+          </TechStackCard>
+        </div>
+
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default HomePage;
