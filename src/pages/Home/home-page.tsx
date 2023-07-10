22792fb7535acc30f611265323aa74da06a7e15f
nativacu <nativacu@gmail.com>
2023-07-10 09:17:14 -0400

    feat: adding app title helper component

--- src/pages/Home/home-page.tsx
+++ src/pages/Home/home-page.tsx
@@ -1,11 +1,13 @@
 import { IonContent, IonHeader, IonPage } from '@ionic/react';
 import { TechStackCard } from '../../components/TechStackCard/tech-stack-card';
+import { AppTitle } from '../../components/AppTitle/app-title';
 import './home-page.scss';
 
 const HomePage: React.FC = () => {
   return (
     <IonPage>
       <IonHeader className="no-shadow-header">
+        <AppTitle/>
       </IonHeader>
       <IonContent fullscreen>
         <div className='ion-flex'>
