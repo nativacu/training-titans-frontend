9f8f67b30e382992869685f9c6aff305884a216e
Robert Romero <robert02frontend@gmail.com>
2023-07-26 09:04:53 -0400

    new styles in this transcript call

--- src/pages/Call/components/transcript-call/transcript-call.tsx
+++ src/pages/Call/components/transcript-call/transcript-call.tsx
@@ -8,15 +8,23 @@ import { IonCol, IonContent, IonHeader, IonPage, IonRow } from "@ionic/react";
 const TranscriptCall = () => {
   const transcript = [
     {
-      type: "You",
+      type: "you",
       text: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
     },
     {
-      type: "Candidate",
+      type: "candidate",
       text: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
     },
     {
-      type: "You",
+      type: "you",
+      text: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+    },
+    {
+      type: "candidate",
+      text: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+    },
+    {
+      type: "you",
       text: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
     },
   ];
@@ -24,12 +32,38 @@ const TranscriptCall = () => {
   return (
     <div className="section">
       <h2 className="subtitle">Transcript</h2>
-      {transcript.map((item, index) => (
-        <div className="card" key={index}>
-          <h3>{item.type}</h3>
-          <div className="content">{item.text}</div>
-        </div>
-      ))}
+
+      <div className="content-transcript">
+        {transcript.map((item, index) => (
+          <div className={`card `} key={index}>
+            {/* <h3>{item.type}</h3> */}
+            <div className={`content ${item.type === "you" && "you"}`}>
+              {item.text}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <IonRow
+        style={{
+          justifyContent: "center",
+          alignItems: "Center",
+          marginTop: "2em",
+        }}
+      >
+        <IonCol size="10">
+          <input
+            type="text"
+            className="input"
+            placeholder="Write something..."
+          />
+        </IonCol>
+        <IonCol size="2">
+          <IonButton size="default" color="primary" style={{ width: "100%" }}>
+            <IonIcon slot="start" icon={sendOutline} /> Send
+          </IonButton>
+        </IonCol>
+      </IonRow>
     </div>
   );
 };
