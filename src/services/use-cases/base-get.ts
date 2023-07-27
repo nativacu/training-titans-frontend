71b2d227a494d8bde36f325a980bc93044f97289
Lissette Luciano <lizandra_016@hotmail.com>
2023-07-27 13:23:16 -0400

    Feat: Connect backend endpoints

---
+++ src/services/use-cases/base-get.ts
@@ -0,0 +1,3 @@
+export interface BaseGet<T> {
+    data: T
+}
---
+++ src/services/use-cases/get-technologies.ts
@@ -0,0 +1,5 @@
+import { Technology } from "../../common/types/TechStack";
+
+export interface GetTechnologiesResult {
+    technologies: Technology[]
+}
